975156d4cace2a5b65e45bd0a41d88edd33f998a
LeoROBERT-FR <contact.robert.leo@gmail.com>
2025-04-04 11:41:18 +0200

    mise à jour #2 du 04/04/2025

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import "../styles/Card.css";
+
+function ProductDetails() {
+    const { id } = useParams();
+    const [produits, setProduits] = useState([]);
+
+    useEffect(() => {
+        const fetchProduits = async () => {
+            try {
+                const response = await axios.get(
+                    `https://api-cafthe.sacha.allardin.dev-campus.fr/api/produits/${id}`,
+                );
+                setProduits(response.data);
+            } catch (error) {
+                console.error("Erreur de chargement des produits ", error);
+            }
+        };
+        void fetchProduits();
+    }, [id]);
+
+    return (
+        <div>
+            <div>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    <div
+                        className={`product-card product-card-${produits.tag}`}
+                        style={{
+                            width: "95%",
+                            marginBottom: "5px",
+                            height: "fit-content",
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-between",
+                                width: "100%",
+                            }}
+                        >
+                            <div
+                                style={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    textAlign: "start",
+                                    margin: "10px 5px",
+                                    width: "100%",
+                                }}
+                            >
+                                <Link to={`/products`} className="btn btn2R">
+                                    ◀︎ Retour
+                                </Link>
+                            </div>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "center",
+                                    width: "100%",
+                                }}
+                            >
+                                <div>
+                                    <h2>{produits.Designation_Article}</h2>
+                                </div>
+                                <div>
+                                    <p>Prix : {produits.Prix_unitaire_Article} €</p>
+                                    <p>{produits.Description_Article}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductDetails;
